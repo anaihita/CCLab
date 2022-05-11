17444e8a2e96668026c463da49f440b87ce40651
anaihita <99340697+anaihita@users.noreply.github.com>
2022-05-11 05:42:56 +0530

    update

---
+++ Instructions/sketch.js
@@ -0,0 +1,23 @@
+let data = "INSTRUCTIONS"
+
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent(canvasContainer);
+  textSize(50)
+  textFont('Start 2P');
+  typeWriter(data, 0, 50, 50, 180);
+}
+
+function draw() {
+}
+
+function typeWriter(sentence, p, x, y, speed) {
+  if (p < (sentence.length)) {
+    text(sentence.substring(0, p + 1), x, y);
+    p++;
+    setTimeout(function () {
+      typeWriter(sentence, p, x, y, speed)
+    }, speed * 3);
+  }
+}
+
